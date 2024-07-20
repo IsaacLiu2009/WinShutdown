676688793e53ca0bf890360b40bfb8f000c7078a
IsaacLiu2009 <87062174+IsaacLiu2009@users.noreply.github.com>
2024-07-21 02:13:24 +0800

    Add files via upload

--- src/i18n.hpp
+++ src/i18n.hpp
@@ -8,7 +8,7 @@
 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
@@ -16,37 +16,65 @@
 
 #ifndef WIN_SHUTDOWN_I18N_8596c30e_69e2_4a04_ba76_678bc422f136
 #define WIN_SHUTDOWN_I18N_8596c30e_69e2_4a04_ba76_678bc422f136
-#include <cstdio>
-#include <cstring>
+#include "io.hpp"
 #include <string>
 #include <unordered_map>
 #define MINI_CASE_SENSITIVE
 #include "mini/ini.h"
 class I18n {
     std::unordered_map<std::string, std::string> l10n;
-    const char *getLangFileName() {
+    std::string getLangFileName() {
         FILE *fp = fopen("language.txt", "r");
-        char name[20];
-        fgets(name, 11, fp);
+        std::string fileName;
+        fgets(fileName, fp);
         fclose(fp);
-        return strcat(name, ".ini");
+        return fileName.append(".ini");
     }
-    void inputL10nFile() {
-        mINI::INIFile l10nFile(this->getLangFileName());
+    void loadLangFile(const std::string &&fileName) {
         mINI::INIStructure l10nData;
-        l10nFile.read(l10nData);
-        for (const auto &section: l10nData) for (const auto &l10nPair: section.second){
-            l10n[l10nPair.first] = l10nPair.second;
-        }
+        (mINI::INIFile(fileName)).read(l10nData);
+        for (const auto &trans: l10nData["message"]){
+            const std::string &unlocalizedName = trans.first;
+            std::string localizedName;
+            size_t i;
+            for (i = 0; trans.second[i] != '"' && i < trans.second.size(); i++);
+            for (i++; trans.second[i] != '"' && i < trans.second.size(); i++){
+                if (trans.second[i] != '\\') localizedName.push_back(trans.second[i]);
+                else switch (trans.second[++i]){
+                    case '"': {
+                        localizedName.push_back('"');
+                        break;
+                    }
+                    case '\\': {
+                        localizedName.push_back('\\');
+                        break;
+                    }
+                    case 'n': {
+                        localizedName.push_back('\n');
+                        break;
+                    }
+                    case 't': {
+                        localizedName.push_back('t');
+                        break;
+                    }
+                }
+            }
+            printf("%s = %s\n", unlocalizedName.c_str(), localizedName.c_str());
+            if (!localizedName.empty() && this->l10n.count(unlocalizedName) == 0)
+                this->l10n.emplace(unlocalizedName, localizedName);
+        }        
     }
 public:
-    std::string getL10n(std::string unlocalizedName) {
-        if (this->l10n.count(unlocalizedName) == 0) return unlocalizedName;
+    I18n() {
+        loadLangFile(this->getLangFileName());
+    }
+    const std::string& getL10n(const std::string &unlocalizedName) {
+        if (!this->l10n.count(unlocalizedName) == 0) return unlocalizedName;
         return this->l10n[unlocalizedName];
     }
     const char *getL10n(const char *unlocalizedName) {
-        if (this->l10n.count(std::string(unlocalizedName)) == 0) return unlocalizedName;
-        return this->l10n[std::string(unlocalizedName)].c_str();
+        if (!this->l10n.count(unlocalizedName) == 0) return unlocalizedName;
+        return this->l10n[unlocalizedName].c_str();
     }
 };
 #endif
